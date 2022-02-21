1cdfa5219bc50a1d49a18521fca3e4459f712882
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-21 12:46:11 -0800

    added form event listener

--- create/create.js
+++ create/create.js
@@ -1,38 +1,38 @@
-import { 
-    createBunny, 
-    getFamilies, 
-    checkAuth, 
-    logout 
-} from '../fetch-utils.js';
+import { createBunny, getFamilies, checkAuth, logout } from '../fetch-utils.js';
 
 const form = document.querySelector('.bunny-form');
 const logoutButton = document.getElementById('logout');
 
-form.addEventListener('submit', async e => {
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const familyId = formData.get('family-id');
+    const name = formData.get('bunny-name');
+
+    await createBunny({
+        name: name,
+        family_id: familyId,
+    });
     // prevent default
 
     // get the name and family id from the form
 
     // use createBunny to create a bunny with this name and family id
-    
+
     form.reset();
 });
 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
-
     // go get the families from supabase
-
     // for each family
-
     // create an option tag
-
     // set the option's value and text content
-
     // and append the option to the select
 });
-
 
 checkAuth();
 
