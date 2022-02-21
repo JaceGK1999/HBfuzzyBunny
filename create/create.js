7ecbcbc03ceed118a1fe02860762865abb19a8fd
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-21 12:47:19 -0800

    added window even listener

--- create/create.js
+++ create/create.js
@@ -25,6 +25,17 @@ form.addEventListener('submit', async (e) => {
 });
 
 window.addEventListener('load', async () => {
+    const select = document.querySelector('select');
+    const families = await getFamilies();
+
+    for (let family of families) {
+        const option = document.createElement('option');
+
+        option.value = family.id;
+        option.textContent = family.name;
+
+        select.append(option);
+    }
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
     // go get the families from supabase
